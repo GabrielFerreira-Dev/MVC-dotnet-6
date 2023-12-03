164844706d4f8d7031a3610561b2c8e9340c03ea
Gabriel Ferreira <gabrieel.ferreira55@gmail.com>
2023-12-03 17:00:28 -0300

    Criação de Formulário Simples para Criar Vendedor

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -12,5 +12,10 @@ namespace SalesWebMVC.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller) {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
