1dd992bacc2dd6446ef7bed8324a6eb8f09a83a1
Gabriel Ferreira <gabrieel.ferreira55@gmail.com>
2023-12-03 13:15:38 -0300

    Criação de Seeding Service

---
+++ SalesWebMVC/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using SalesWebMVC.Data;
+using System.Configuration;
+
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+    options.UseMySql("server=localhost;initial catalog=SALES_WEB_MVC;uid=developer;pwd=123456",
+        ServerVersion.Parse("8.0.34-mysql")));
+
+builder.Services.AddScoped<SeedingService>();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+} else {
+    using (var scope = app.Services.CreateScope()) {
+        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
